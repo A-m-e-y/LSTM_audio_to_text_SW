af304da90a53f919cd8174d937a6e4968aa958b1
A-m-e-y <ameyk1510@gmail.com>
2025-04-26 20:02:37 -0700

    Implemented custom LSTM cell in tensorflow. This is done in order to understand the inner workings of LSTM cells. Also, this will make it easy to write HW accelerator for it later.
    Changes to be committed:
    	modified:   custom_lstm_cell.py

--- custom_lstm_cell.py
+++ custom_lstm_cell.py
@@ -1,41 +1,38 @@
 import tensorflow as tf
 import numpy as np
+from keras.saving import register_keras_serializable
 
+@register_keras_serializable()
 class CustomLSTMCell(tf.keras.layers.Layer):
-    def __init__(self, units):
-        super(CustomLSTMCell, self).__init__()
+    def __init__(self, units, **kwargs):
+        super(CustomLSTMCell, self).__init__(**kwargs)
         self.units = units
+        self.state_size = [self.units, self.units]
+        self.output_size = self.units
 
     def build(self, input_shape):
         input_dim = input_shape[-1]
 
-        # Create weights for input and hidden state separately
         self.kernel = self.add_weight(shape=(input_dim, self.units * 4), initializer='glorot_uniform', name='kernel')
         self.recurrent_kernel = self.add_weight(shape=(self.units, self.units * 4), initializer='orthogonal', name='recurrent_kernel')
         self.bias = self.add_weight(shape=(self.units * 4,), initializer='zeros', name='bias')
 
     def sw_dot(self, x, w):
-        """Software dot product using NumPy for now."""
-        # x: (batch_size, input_dim)
-        # w: (input_dim, units*4)
+        """Software dot product."""
         return tf.linalg.matmul(x, w)
 
     def call(self, inputs, states):
-        h_tm1, c_tm1 = states  # previous hidden and cell states
+        h_tm1, c_tm1 = states
 
-        # Calculate z = x * Wx + h_tm1 * Wh + b
         z = self.sw_dot(inputs, self.kernel) + self.sw_dot(h_tm1, self.recurrent_kernel) + self.bias
 
-        # Split z into 4 parts for gates
         z0, z1, z2, z3 = tf.split(z, num_or_size_splits=4, axis=1)
 
-        # Gates
-        i = tf.sigmoid(z0)          # Input gate
-        f = tf.sigmoid(z1)          # Forget gate
-        g = tf.tanh(z2)             # Candidate memory
-        o = tf.sigmoid(z3)          # Output gate
+        i = tf.sigmoid(z0)
+        f = tf.sigmoid(z1)
+        g = tf.tanh(z2)
+        o = tf.sigmoid(z3)
 
-        # New cell state and hidden state
         c = f * c_tm1 + i * g
         h = o * tf.tanh(c)
 
